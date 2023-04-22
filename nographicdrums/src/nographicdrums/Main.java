ffc0840c0dfbf7d5141f84d90059d8d447ddef60
donat <donat@192.168.1.54>
2023-04-22 15:21:01 +0200

    Message for nongraphicdrums

---
+++ nographicdrums/src/nographicdrums/Main.java
@@ -0,0 +1,10 @@
+package nographicdrums;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("No graphic drums!!!");
+	}
+
+}
