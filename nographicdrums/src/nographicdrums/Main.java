78f66cb0b8e377a08b59636b9fe1bf53d5ca9a63
donat <donat@192.168.1.54>
2023-04-23 10:46:38 +0200

    added you see

--- nographicdrums/src/nographicdrums/Main.java
+++ nographicdrums/src/nographicdrums/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("No graphic drums!");
+System.out.println("No graphic drums,you see!");
 	}
 
 }
